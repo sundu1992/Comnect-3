6627a784b444053a5fd9d0f967064c885bbb5e77
Sundari Selvarajan <sundari_selva@hotmail.com>
2016-06-16 17:19:32 -0400

    Third Commit ---- Check win (in logs)

--- app/src/main/java/com/example/sundu/connect3/MainActivity.java
+++ app/src/main/java/com/example/sundu/connect3/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     // 0 - O and 1-X
     int activePlayer = 0;
     int[] gameState = {2,2,2,2,2,2,2,2,2}; // 2 - unplayed
+    int [][] winningPosition = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,4,8},{1,5,8},{2,4,6}};
 
     public void dropIn(View view){
 
@@ -30,6 +31,15 @@ public class MainActivity extends AppCompatActivity {
                 activePlayer = 0;
             }
             counter.animate().translationYBy(1000f).setDuration(300);
+            for(int[] winPost : winningPosition){
+
+                if(gameState[winPost[0]] == gameState[winPost[1]] &&
+                        gameState[winPost[1]]== gameState[winPost[2]] &&
+                        gameState[winPost[0]] != 2){
+                    System.out.println(gameState[winPost[0]]);
+
+                }
+            }
         }
 
     }
